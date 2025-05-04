da1f1266f08c762824dd97938bb8dfa1a605f554
SanjCorp <richisanj@gmail.com>
2025-05-04 19:12:14 -0400

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,4 @@
-// Display current year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-// Display last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("year").textContent = new Date().getFullYear();
+  document.getElementById("lastModified").textContent = document.lastModified;
+});
