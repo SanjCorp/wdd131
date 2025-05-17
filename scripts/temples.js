6e27b8493b72620e0c1ceae84ccb42abf90d638e
SanjCorp <richisanj@gmail.com>
2025-05-17 15:07:08 -0400

    Update temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,13 @@
-// Año actual
-const yearSpan = document.getElementById("year");
-yearSpan.textContent = new Date().getFullYear();
+// Menu toggle
+const menuButton = document.getElementById('menu');
+const navigation = document.querySelector('.navigation');
 
-// Última modificación
-const lastModifiedParagraph = document.getElementById("lastModified");
-lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
+menuButton.addEventListener('click', () => {
+  navigation.classList.toggle('show');
+});
+
+// Footer dynamic year
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Last modified
+document.getElementById('lastModified').textContent = document.lastModified;
