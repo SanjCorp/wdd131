4d6a73a20d0337f23d9f8cdf27d995242b3bc11c
SanjCorp <richisanj@gmail.com>
2025-06-02 21:17:58 -0400

    Update place.js

--- scripts/place.js
+++ scripts/place.js
@@ -1,25 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const year = document.getElementById("year");
-  const lastModified = document.getElementById("lastModified");
-  year.textContent = new Date().getFullYear();
-  lastModified.textContent = document.lastModified;
+// Footer dinámico
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
 
-  const temp = parseFloat(document.getElementById("temp").textContent);
-  const wind = parseFloat(document.getElementById("wind").textContent);
-  const windchillEl = document.getElementById("windchill");
+// Menú hamburguesa
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
 
-  function calculateWindChill(tempC, windKmh) {
-    return (
-      13.12 +
-      0.6215 * tempC -
-      11.37 * Math.pow(windKmh, 0.16) +
-      0.3965 * tempC * Math.pow(windKmh, 0.16)
-    ).toFixed(1);
-  }
-
-  if (temp <= 10 && wind > 4.8) {
-    windchillEl.textContent = `${calculateWindChill(temp, wind)} °C`;
-  } else {
-    windchillEl.textContent = "N/A";
-  }
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
 });
