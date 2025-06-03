81a1c8f55b6f018c49265359b0fa1f87d17a626a
SanjCorp <richisanj@gmail.com>
2025-06-02 20:55:43 -0400

    Update place.js

--- scripts/place.js
+++ scripts/place.js
@@ -1,14 +1,25 @@
+function calculateWindChill(temp, speed) {
+  return (
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(speed, 0.16) +
+    0.3965 * temp * Math.pow(speed, 0.16)
+  ).toFixed(1);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  const yearSpan = document.getElementById("year");
-  if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
+  // Fecha actual y última modificación
+  document.getElementById("currentyear").textContent = new Date().getFullYear();
+  document.getElementById("lastmodified").textContent = document.lastModified;
+
+  // Cálculo de windchill
+  const temp = parseFloat(document.getElementById("temperature").textContent);
+  const speed = parseFloat(document.getElementById("windspeed").textContent);
+  const windchill = document.getElementById("windchill");
+
+  if (temp <= 10 && speed > 4.8) {
+    windchill.textContent = calculateWindChill(temp, speed) + " °C";
+  } else {
+    windchill.textContent = "N/A";
   }
-
-  const menuBtn = document.getElementById("menuBtn");
-  const menu = document.getElementById("menu");
-
-  menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-    menu.classList.toggle("show");
-  });
 });
